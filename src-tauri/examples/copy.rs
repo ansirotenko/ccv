5e1aef2d906c6e8a21daaa89169ae9480ad75927
Margarita Loginova <margaritaloginova@Air-Margarita.local>
2024-07-14 02:21:30 +0300

    fix mac

--- src-tauri/examples/copy.rs
+++ src-tauri/examples/copy.rs
@@ -2,12 +2,34 @@
 
 
 use clipboard_master::{Master, ClipboardHandler, CallbackResult};
+
 use std::io;
 use arboard::{Clipboard};
 use clipboard_files::{read};
+use objc2::{
+	msg_send_id,
+	rc::{autoreleasepool, Id},
+	runtime::ProtocolObject,
+	ClassType,
+};
+use objc2_app_kit::{NSPasteboard, NSPasteboardTypeHTML, NSPasteboardTypeString};
+use objc2_foundation::{NSArray, NSString, NSURL};
+use std::{
+	borrow::Cow,
+	panic::{RefUnwindSafe, UnwindSafe},
+};
 
-#[cfg(target_os = "macos")]
-mod mac;
+pub fn paste(s: &str) {
+    let pasteboardOtp: Option<Id<NSPasteboard>> =
+			unsafe { msg_send_id![NSPasteboard::class(), generalPasteboard] };
+
+    let pasteboard: Id<NSPasteboard> = pasteboardOtp.unwrap();
+
+    unsafe { pasteboard.clearContents() };
+	let nsurl = unsafe { NSURL::fileURLWithPath(&NSString::from_str(s)) };
+    let url_array = NSArray::from_vec(vec![ProtocolObject::from_id(nsurl)]);
+	unsafe { pasteboard.writeObjects(&url_array) };
+}
 
 struct Handler{
 }
@@ -24,16 +46,19 @@ impl ClipboardHandler for Handler {
 
         // let content = self.ctx.get_contents();
         // let a = content.unwrap();
+        // /Users/margaritaloginova/dev/ccv/README.md
 
         if read().is_ok() {
             println!("was files");
-        } {
+        } else {
             let mut clipboard = Clipboard::new().unwrap();
             if clipboard.get_image().is_ok() {
                 println!("was image");
             } else {
-                if clipboard.get_text().is_ok() {
+                let text = clipboard.get_text();
+                if let Ok(textData) = text {
                     println!("was text");
+                    paste(&textData);
                 }
             }
         }
