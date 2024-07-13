3d9867f688db82e235eb0020e1c71ebeaece4317
Andrei Sirotenko <panshirotenski@gmail.com>
2024-07-13 16:42:29 +0300

    copy sample

---
+++ src-tauri/examples/copy.rs
@@ -0,0 +1,49 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+
+use clipboard_master::{Master, ClipboardHandler, CallbackResult};
+use std::io;
+use arboard::{Clipboard};
+use clipboard_files::{read};
+
+struct Handler{
+}
+
+impl Handler {
+    fn new() -> Self {
+        Handler {}
+    }
+}
+
+impl ClipboardHandler for Handler {
+    fn on_clipboard_change(&mut self) -> CallbackResult {
+        println!("Clipboard change happened!");
+
+        // let content = self.ctx.get_contents();
+        // let a = content.unwrap();
+
+        if read().is_ok() {
+            println!("was files");
+        } {
+            let mut clipboard = Clipboard::new().unwrap();
+            if clipboard.get_image().is_ok() {
+                println!("was image");
+            } else {
+                if clipboard.get_text().is_ok() {
+                    println!("was text");
+                }
+            }
+        }
+
+        CallbackResult::Next
+    }
+
+    fn on_clipboard_error(&mut self, error: io::Error) -> CallbackResult {
+        eprintln!("Error: {}", error);
+        CallbackResult::Next
+    }
+}
+
+fn main() {
+    Master::new(Handler::new()).run().unwrap();
+}
