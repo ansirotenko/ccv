7d3bc82ee01edb7c12939674413372d5b070c301
Andrei Sirotenko <panshirotenski@gmail.com>
2024-09-22 17:08:21 +0300

    almost there

--- src-tauri/ccv/src/settings/setup.rs
+++ src-tauri/ccv/src/settings/setup.rs
@@ -19,9 +19,28 @@ pub fn read_settings_and_register_keybindings(
     }
 
     let settings = settings.clone().unwrap();
-    if let Err(err) = settings::core::register_keybindings(app_handle, &settings) {
+    if let Err(err) = settings::core::register_keybindings(app_handle, &settings, &None) {
         log::error!("Unable to register initial shortcuts. {err}");
     }
+
+    // TODO change to linux
+    #[cfg(target_os = "linux")]
+    {
+        use global_hotkey::GlobalHotKeyEvent;
+        use tauri::async_runtime;
+
+        let primary_window = app_handle.get_window(crate::primary::SCREEN);
+        
+        async_runtime::spawn(async move {
+            loop {
+                if let Ok(_) = GlobalHotKeyEvent::receiver().try_recv() {
+                    if let Err(err) = crate::utils::window::show_window(&primary_window) {
+                        log::error!("Unable to show primary window. {err}");
+                    }
+                }
+            }
+        });
+    } 
 
     Ok(())
 }
