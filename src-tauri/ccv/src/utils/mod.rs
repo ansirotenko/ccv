00487c92bf339367ddd7fe7a494585c5adc15804
Andrei Sirotenko <ansirotenko@yandex-team.ru>
2024-11-16 00:20:17 +0300

    add comment

--- src-tauri/ccv/src/utils/mod.rs
+++ src-tauri/ccv/src/utils/mod.rs
@@ -11,13 +11,18 @@ pub const WINDOW_HIDDEN_EVENT: &str = "window-hidden";
 pub fn get_app_data_dir(app_handle: &AppHandle) -> Result<PathBuf, AppError> {
     #[cfg(target_os = "linux")]
     {
+        // by default XDG_DATA_HOME environment variable is used instead of HOME environment variable
+        // In case application was started by desktop-launch script, this variable is set to
+        // export XDG_CONFIG_HOME=$SNAP_USER_DATA/.config
+        // This leads to new settings.json and ccv.db file at every new installation with snap
+        // https://forum.snapcraft.io/t/is-xdg-data-home-modified-by-snaps-adapters/17826/7
         return match dirs_sys::home_dir() {
             None => Err(app_error!("Unable to get path to $HOME directory")),
             Some(home_dir) => Ok(home_dir.join(".local/share").join(app_handle.config().identifier.as_str()))
         }
     }
 
-    #[cfg(not(target_os = "linux"))]
+    //#[cfg(not(target_os = "linux"))]
     {
         app_handle
         .path()
