4beb8a540bd2c2fbda1dab7b03ba9ad4a3d14a4e
Andrei Sirotenko <panshirotenski@gmail.com>
2024-11-16 14:22:31 +0300

    cowboy debug

--- src-tauri/ccv/src/utils/mod.rs
+++ src-tauri/ccv/src/utils/mod.rs
@@ -9,6 +9,12 @@ pub const WINDOW_SHOWN_EVENT: &str = "window-shown";
 pub const WINDOW_HIDDEN_EVENT: &str = "window-hidden";
 
 pub fn get_app_data_dir(app_handle: &AppHandle) -> Result<PathBuf, AppError> {
+    //TODO REMOVE THIS LATER
+    log::warn!("XDG_DATA_HOME {:?}", std::env::var_os("XDG_DATA_HOME"));
+    log::warn!("HOME {:?}", std::env::var_os("HOME"));
+    log::warn!("SUC {:?}", std::env::var_os("SUC"));
+    log::warn!("SRH {:?}", std::env::var_os("SRH"));
+
     app_handle
         .path()
         .app_data_dir()
