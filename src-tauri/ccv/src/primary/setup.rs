68a525312b3d360b3a32714c5f683e07cb39de0a
Andrei Sirotenko <panshirotenski@gmail.com>
2024-11-16 19:53:16 +0300

    yet another attempt

--- src-tauri/ccv/src/primary/setup.rs
+++ src-tauri/ccv/src/primary/setup.rs
@@ -12,7 +12,7 @@ pub fn init_repository(app_handle: &AppHandle) -> Result<(), AppError> {
     {
         let app_data_dir = get_app_data_dir(app_handle)?;
         let database_path = app_data_dir.join("ccv.db");
-        log::warn!("Database path {:?}", database_path);
+         // TODO GET RID OF THISlog::warn!("Database path {:?}", database_path);
 
         let sqlite_repo =
             ccv_sqlite::repository::SqliteRepository::new(database_path.to_str().unwrap())?;
